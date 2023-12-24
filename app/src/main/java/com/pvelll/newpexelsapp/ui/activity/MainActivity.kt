0ff15c851b7010b580620816de5e9a8083e3265b
Pavel Sushko <nintsao@gmail.com>
2023-12-24 23:23:53 +0300

    added navigation in aplication

--- app/src/main/java/com/pvelll/newpexelsapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/pvelll/newpexelsapp/ui/activity/MainActivity.kt
@@ -2,15 +2,18 @@ package com.pvelll.newpexelsapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.findNavController
 import com.pvelll.newpexelsapp.R
 import com.pvelll.newpexelsapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    //    private val navController = rememberNavController()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen().setKeepOnScreenCondition() {
@@ -18,6 +21,6 @@ class MainActivity : AppCompatActivity() {
         }
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
+
 }
---
+++ app/src/main/java/com/pvelll/newpexelsapp/ui/viewmodels/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.pvelll.newpexelsapp.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
